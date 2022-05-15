8c8dc58dcf3583e8127283949f53dce59c6d0a26
songjaekyung <jaek81@gmail.com>
2022-05-15 02:34:05 +0000

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+import ssl
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+     return render_template('index.html')
+
+if __name__ == '__main__':
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='/etc/ssl/songjaekyung.crt', keyfile='/etc/ssl/songjaekyung.key', password='10244')
+    app.run(host="0.0.0.0", port=443, ssl_context=ssl_context)
